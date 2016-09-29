db5ea2f6dd1ce51cf74b37d8aa4831f856575ab7
Jon Bakies <jon@baki.es>
2016-09-29 13:53:08 -0400

    added triangle numbers function

--- la0.hs
+++ la0.hs
@@ -1,2 +1,3 @@
 add_and_triple x y = (x + y) * 3 -- takes two numbers, adds them together then triples the result
-(+*) x y = add_and_triple x y
+(+*) x y = add_and_triple x y 	-- infix operator to call add_and_triple
+triangle_numbers l = [(x * (x + 1)) `div` 2 | x <- [1..l]] -- generates a list of first l triangle numbers 
