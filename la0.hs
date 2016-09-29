858c07023ae00be3116c32911475a2b72158f408
Jon Bakies <jon@baki.es>
2016-09-29 13:06:40 -0400

    Removed provided comments, added my own

--- la0.hs
+++ la0.hs
@@ -1,5 +1,1 @@
--- This is the file where you should write your functions to turn in
-add_and_triple x y = (x + y) * 3
--- Comments look like this. Please add comments to describe your code
-
-
+add_and_triple x y = (x + y) * 3 -- takes two numbers, adds them together then triples the result
